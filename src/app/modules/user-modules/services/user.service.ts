0b875de826910799061c54fa8a3d6cc185533c82
devendra <devendra@devendras-MacBook-Pro.local>
2023-08-06 23:59:19 +0530

    Feat: list user api integration

--- src/app/modules/user-modules/services/user.service.ts
+++ src/app/modules/user-modules/services/user.service.ts
@@ -1,36 +1,24 @@
 import { Injectable, } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-import { AuthService, HttpService } from "src/app/core";
-import { ConfigService } from "src/app/shared";
+import { HttpService } from "src/app/core";
+import { ConfigService, ServerResponse, RequestParam } from "src/app/shared";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 // userService.ts
 @Injectable({ providedIn: 'root' })
 export class UserService extends HttpService{
   override baseUrl: string;
-  constructor(private authService: AuthService, private configService: ConfigService, http: HttpClient) {
+  constructor(private configService: ConfigService, http: HttpClient) {
     super(http);
-    this.baseUrl = "";
+    this.baseUrl = environment.apiUrl;
   }
 
-  getUserRoles(): string[] {
-    const token = this.authService.getToken();
-    if (token) {
-    }
-    return [this.configService.rolesConfig.ROLES.GRIEVANCE_NODAL];
-  }
-
-  login() {
-
-  }
-
-  isAdmin(): boolean {
-    const roles = this.getUserRoles();
-    return roles.includes('Admin');
-  }
+  getAllUsers(): Observable<ServerResponse>  {
+    const  reqParam: RequestParam = { url: this.configService.urlConFig.URLS.GET_ALL_USERS}
+    return this.get(reqParam);
+  } 
 
   // Implement methods for other roles (e.g., isGrievanceNodal(), isNodalOfficer(), isSecretary())
-}
-function jwt_decode<T>(token: string) {
-  throw new Error("Function not implemented.");
 }
 
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -2,3 +2,4 @@ export * from './shared.module';
 export * from './interfaces';
 export * from './services/config/config.service';
 export * from './config';
+export * from './util';
