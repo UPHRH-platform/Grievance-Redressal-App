7dcab651d5f5f35d45cf3dc2b997ef2471dd7490
Roopashree <roopashree.lakshmikeshava@tarento.com>
2023-08-14 14:49:53 +0530

    dashboard UI and integration - partial commit

--- src/app/modules/dashboard/dashboard-view/dashboard-view.component.ts
+++ src/app/modules/dashboard/dashboard-view/dashboard-view.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { TableColumn, DashboardTableData, DashboardAnalytics } from '../../../interfaces/interfaces';
-import { BreadcrumbItem } from 'src/app/shared';
+import { BreadcrumbItem, ConfigService } from 'src/app/shared';
+import { DashboardService } from '../services/dashboard.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-view',
@@ -9,156 +11,125 @@ import { BreadcrumbItem } from 'src/app/shared';
 })
 export class DashboardViewComponent {
   isDataLoading : boolean = false;
-  dashboardData: DashboardTableData[] = [];
-  dashboardDataColumns: TableColumn[] = [];
-  dashboardAnalyticsData: DashboardAnalytics[] = [];
-  dashboardAnalyticsColumns: TableColumn[] = [];
-  dashboardAnaytics1Columns: TableColumn[] = [];
-  dashboardAnalytics1Data: any[] = [];
-  dashboardAnaytics2Columns: TableColumn[] = [];
-  dashboardAnalytics2Data: any[] = [];
-  dashboardAnalytics3Columns: TableColumn[] = [];
-  dashboardAnalytics3Data: any[] = [];
+  dashboardData: any = {};
   breadcrumbItems: BreadcrumbItem[] = [
     { label: 'Grievance Management', url: '/home' },
     { label: 'Dashboard', url: '/dashboard' },
   ];
+  assignmentMatrixColumns: any = [];
+  assignmentMatrixData: any = [];
+  performanceIndicatorData: any = {};
+  resolutionMatrixData: any = [];
+  resolutionMatrixColumns: any = [];
+  grievancesTypes:any[]=[];
+  public assignGrievanceTypeForm:FormGroup;
 
-  constructor() {}
+  constructor(private dashboardService: DashboardService, private configService: ConfigService, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.grievancesTypes = this.configService.dropDownConfig.GRIEVANCE_TYPES;
     this.initializeColumns();
     this.getDashboardData();
+    this.createAssignForm();
+  }
+
+  createAssignForm(){
+    this.assignGrievanceTypeForm = this.formBuilder.group({
+      grievanceType: new FormControl('')
+    })
   }
 
   initializeColumns(): void {
-    this.dashboardDataColumns = [
+    this.assignmentMatrixColumns = [
       {
         columnDef: 'id',
         header: '#',
-        isSortable: true,
+        isSortable: false,
         isLink: false,
         cell: (element: Record<string, any>) => `${element['id']}`
-      },
+},
       {
-        columnDef: 'bucket',
-        header: 'Bucket',
-        isSortable: true,
-        isLink: false,
-        cell: (element: Record<string, any>) => `${element['bucket']}`
-      },
-      {
-        columnDef: 'responsibleOfficer',
-        header: 'Responsible Officer',
-        isSortable: true,
-        isLink: false,
-        cell: (element: Record<string, any>) => `${element['responsibleOfficer']}`
-      },
-      {
-        columnDef: 'number',
-        header: 'Number',
-        isSortable: true,
-        isLink: false,
-        cell: (element: Record<string, any>) => `${element['number']}`
-      },
-      {
-        columnDef: 'pending',
-        header: 'Pending',
-        isSortable: true,
-        isLink: false,
-        cell: (element: Record<string, any>) => `${element['pending']}`
-      },
-      {
-        columnDef: 'In-Process',
-        header: 'inProcess',
-        isSortable: true,
-        isLink: false,
-        cell: (element: Record<string, any>) => `${element['inProcess']}`
-      },
-      {
-        columnDef: 'resolved',
-        header: 'Resolved',
-        isSortable: true,
-        isLink: false,
-        cell: (element: Record<string, any>) => `${element['resolved']}`
-      },
-      {
-        columnDef: 'responseNotNeeded',
-        header: 'Response not needed',
-        isSortable: true,
-        isLink: false,
-        cell: (element: Record<string, any>) => `${element['responseNotNeeded']}`
-      },
-      {
-        columnDef: 'duplicate',
-        header: 'Duplicate',
-        isSortable: true,
-        isLink: false,
-        cell: (element: Record<string, any>) => `${element['duplicate']}`
-      }
-    ];
-    this.dashboardAnalyticsColumns = [
-      {
-        columnDef: 'status',
-        header: 'Status',
-        cell: (element: Record<string, any>) => `Status`
-      },
-      {
-        columnDef: 'pending',
-        header: 'Pending',
-        cell: (element: Record<string, any>) => `${element['pending']}`
-      },
-      {
-        columnDef: 'inProcess',
-        header: 'In-Process',
-        cell: (element: Record<string, any>) => `${element['inProcess']}`
-      },
-      {
-        columnDef: 'Resolved',
-        header: 'Resolved',
-        cell: (element: Record<string, any>) => `${element['resolved']}`
-      },
-      {
-        columnDef: 'responseNotNeeded',
-        header: 'Response not needed',
-        cell: (element: Record<string, any>) => `${element['responseNotNeeded']}`
-      },
-      {
-        columnDef: 'duplicate',
-        header: 'Duplicate',
-        cell: (element: Record<string,any>) => `${element['duplicate']}`
-      }
+                columnDef: 'tag',
+                header: 'Tag',
+                isSortable: false,
+                isLink: false,
+                cell: (element: Record<string, any>) => `${element['tag']}`
+    },
+    {
+      columnDef: 'value',
+      header: 'Number of tickets',
+      isSortable: false,
+      isLink: false,
+      cell: (element: Record<string, any>) => `${element['value']}`
+},
+
     ]
-    this.dashboardAnaytics1Columns = [
+    this.resolutionMatrixColumns = [
       {
-        columnDef: 'noOfIssuesResolved',
-        header: '# of issues resolved',
-        cell: (element: Record<string, any>) => `${element['noOfIssuesResolved']}`
-      },
+        columnDef: 'id',
+        header: '#',
+        isSortable: false,
+        isLink: false,
+        cell: (element: Record<string, any>) => `${element['id']}`
+},
       {
-        columnDef: 'statusNotUpdated',
-        header: 'Status not updated',
-        cell: (element: Record<string, any>) => `${element['statusNotUpdated']}`
-      }
-    ];
-    this.dashboardAnaytics2Columns = [
-      {
-        columnDef: 'turnAroundTime',
-        header: 'Turn around time',
-        cell: (element: Record<string, any>) => `${element['turnAroundTime']}`
-      }, 
-      {
-        columnDef: 'noOfGrievances',
-        header: '# of Grievances',
-        cell: (element: Record<string, any>) => `${element['noOfGrievances']}`
-      }, 
-    ]
-    this.dashboardAnalytics3Columns = [
-      {
-        columnDef: 'avgDaysToResolve',
-        header: 'Avg Days to resolve',
-        cell: (element: Record<string, any>) => `${element['avgDaysToResolve']}`
-      }
+        columnDef: 'departmentName',
+        header: 'Department',
+        isSortable: true,
+        isLink: false,
+        cell: (element: Record<string, any>) => `${element['departmentName']}`
+},
+{
+  columnDef: 'total',
+  header: 'Total Ticket',
+  isSortable: true,
+  isLink: false,
+  cell: (element: Record<string, any>) => `${element['total']}`
+},
+{
+  columnDef: 'isJunk',
+  header: 'Junk Ticket',
+  isSortable: true,
+  isLink: false,
+  cell: (element: Record<string, any>) => `${element['isJunk']}`
+},
+{
+  columnDef: 'isClosed',
+  header: 'Resolved Ticket',
+  isSortable: true,
+  isLink: false,
+  cell: (element: Record<string, any>) => `${element['isClosed']}`
+},
+
+{
+  columnDef: 'isEscalated',
+  header: 'Escalated',
+  isSortable: true,
+  isLink: false,
+  cell: (element: Record<string, any>) => `${element['isEscalated']}`
+},
+
+{
+  columnDef: 'isOpen',
+  header: 'Pending Ticket',
+  isSortable: true,
+  isLink: false,
+  cell: (element: Record<string, any>) => `${element['isOpen']}`
+},
+{
+  columnDef: 'openTicket',
+  header: 'Open Ticket',
+  isSortable: true,
+  isLink: false,
+  cell: (element: Record<string, any>) => `${element['openTicket']}`
+},
+{
+  columnDef: 'unassigned',
+  header: 'Unassigned',
+  isSortable: true,
+  isLink: false,
+  cell: (element: Record<string, any>) => `${element['unassigned']}`
+},
     ]
   }
 
@@ -167,73 +138,37 @@ export class DashboardViewComponent {
       setTimeout(() => {
         this.isDataLoading = false;
       }, 2000);
-      this.dashboardData = [
-        {
-          id: "1",
-          bucket: 'Affiliation',
-          responsibleOfficer: 'Art of living',
-          number: '3 (2%)',
-          pending: '0 (0%)',
-          inProcess: '0 (0%)',
-          resolved: '3 (100%)',
-          responseNotNeeded: '0 (0%)',
-          duplicate: '0 (0%)'
+      this.dashboardData = this.dashboardService.getDashboardData().body;
+      //assignmentmatrix data
+      const tag = [];
+      const value:any = [];
+      for(const key in this.dashboardData.assignmentMatrix) {
+          tag.push(this.camelCaseToWords(key));
+          value.push(this.dashboardData.assignmentMatrix[key])
+      }
+      tag.map((obj, index) => {
+        this.assignmentMatrixData.push({
+          id: index + 1,
+          tag: obj,
+          value: value[index]
+        })
+      })
+      this.dashboardData.resolutionMatrix.map((obj: any, index: number) => {
+        let i = index;
+        for(const key in obj) {
+          i = i + 1;
+          this.resolutionMatrixData.push({id: i, departmentName: this.camelCaseToWords(key), ...obj[key]});
+        }
+      })
+    }
 
-        },
-        {
-          id: "2",
-          bucket: 'Biometric Attendance',
-          responsibleOfficer: 'Microsoft',
-          number: '102 (55%)',
-          pending: '1 (1%)',
-          inProcess: '0 (0%)',
-          resolved: '60 (59%)',
-          responseNotNeeded: '2 (2%)',
-          duplicate: '10 (10%)'
-        },
-        {
-          id: "3",
-          bucket: 'Enrollment',
-          responsibleOfficer: 'ISTM',
-          number: '25 (14%)',
-          pending: '0 (0%)',
-          inProcess: '1 (4%)',
-          resolved: '24 (96%)',
-          responseNotNeeded: '0 (0%)',
-          duplicate: '0 (0%)'
-        },        
-      ];
-      this.dashboardAnalyticsData = [
-        {
-          status: '',
-          pending: '0 (0%)',
-          inProcess: '0 (0%)',
-          resolved: '3 (100%)',
-          responseNotNeeded: '0 (0%)',
-          duplicate: '0 (0%)'
-        },
-      ];
-      this.dashboardAnalytics1Data = [
-        {
-          noOfIssuesResolved: 184,
-          statusNotUpdated: 29
-        }
-      ];
-      this.dashboardAnalytics3Data = [
-        {
-          avgDaysToResolve: 49
-        }
-      ],
-      this.dashboardAnalytics2Data = [
-        {
-          turnAroundTime: '<=7days',
-          noOfGrievances: '56'
-        },
-        {
-          turnAroundTime: '>7days',
-          noOfGrievances: '39'
-        }
-      ]
-      
+    camelCaseToWords(s: string) {
+      const result = s.replace(/([a-zA-Z])([A-Z])([a-z])/g, '$1 $2$3');
+      return result.charAt(0).toUpperCase() + result.slice(1);
+    }
+
+    grievanceSelected(grievance: Event) {
+      console.log(grievance)
     }
 }
+
---
+++ src/app/modules/dashboard/services/dashboard.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ConfigService, RequestParam, Response, ServerResponse } from 'src/app/shared';
+import { environment } from 'src/environments/environment';
+import { HttpService } from "src/app/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService  extends HttpService {
+  override baseUrl: string;
+  constructor(http: HttpClient, private configService: ConfigService) { 
+    super(http);
+    this.baseUrl = environment.apiUrl;
+  }
+
+  getDashboardData(request?:object) {
+    const response = {
+      "status": 200,
+      "body": {
+        "assignmentMatrix": {
+          "total": 7,
+          "isOpen": 7,
+          "isClosed": 7,
+          "isJunk": 4,
+          "isEscalated": 0,
+          "unassigned": 0
+        },
+        "resolutionMatrix": [{
+          "examination": {
+            "total": 7,
+            "isOpen": 7,
+            "isClosed": 7,
+            "isJunk": 4,
+            "isEscalated": 0,
+            "unassigned": 0,
+            "openTicket": ""
+          },
+          "affiliation": {
+            "total": 7,
+            "isOpen": 7,
+            "isClosed": 7,
+            "isJunk": 4,
+            "isEscalated": 0,
+            "unassigned": 0,
+            "openTicket": '',
+          }
+        }],
+        "performanceIndicators": {
+          "turnAroundTime": 20,
+          "escalationPercentage": 10,
+          "nudgeTicketPercentage": 25,
+          "ticketpending": 21
+        }
+      }
+    }
+      // const reqParam: RequestParam = {
+      //   url: this.configService.urlConFig.URLS.GRIEVANCE_TICKETS.GET_DASHBOARD_DATA,
+      //   data: {
+      //     ...request
+      //   }
+      // }
+      // return this.post(reqParam);
+      return response;
+  }
+}
